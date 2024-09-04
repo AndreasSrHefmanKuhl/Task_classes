d66e41579a54fd796a34895a00f824bce11de4f5
AndreasKuhl <hefbolzen@gmail.com>
2024-09-04 13:30:03 +0200

    fin

---
+++ bürgerbüro.dart
@@ -0,0 +1,29 @@
+import 'person_changes.dart';
+
+class CitOffice {
+  List<Person> attPerson = [];
+
+  void changeAdress(Person person, String newAdress) {
+    person.setAdress(newAdress);
+    print("${person.name} wurde an ${newAdress} umgemeldet! " )
+  }
+
+  void personComes(Person person) {
+    attPerson.add(person);
+    print("${person.name} ist ins Büro gekommen! " );
+  }
+
+  void personLeaves(Person person) {
+    attPerson.remove(person);
+    print("${person.name} ist ins Büro verlassen! " );
+  }
+
+  void showActives(){
+    print("Anwesende Personen: ");
+    for(var person in attPerson){
+     print ("- ${person.name}");
+
+    }
+
+  }
+}
