fdf5dd2d30672c272215d6acc14b74f91f576e6b
AndreasKuhl <hefbolzen@gmail.com>
2024-09-04 12:34:09 +0200

    fin

---
+++ person_changes.dart
@@ -0,0 +1,19 @@
+class Person {
+  String name;
+  int age;
+  String adress;
+
+  Person(this.name, this.age, this.adress);
+
+  void setName(String newName) {
+    name = newName;
+  }
+
+  void setAdress(String newAdress) {
+    adress = newAdress;
+  }
+
+  void setAge(int newAge) {
+    age = newAge;
+  }
+}
